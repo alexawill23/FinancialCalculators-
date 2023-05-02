c1c6b8bf12c5a834f78bbce295502c62e5c8ae0b
Alexa Williams <alexawill23@gmail.com>
2023-05-02 03:06:21 -0400

    added new css & modified mortgage calculator

--- scripts/indexmc.js
+++ scripts/indexmc.js
@@ -1,17 +1,22 @@
 "use strict"; 
-console.log("the index.js has begun executing"); 
 
-function calculatebtn (){
+window.onload = init; 
+
+function init(){
+let calculatebtn = document.getElementById("calculatebtn");  
+ calculatebtn.onclick = calculatebtnClicked; 
+
+function calculatebtnClicked (){
     let loanAmount= document.getElementById("loanAmount").value; 
     let interestRate= document.getElementById("interestRate").value; 
     let loanTerm= document.getElementById("loanTerm").value; 
 
-}
+
 
 //monthly payment calculation
 
 let montlyInterestRate= (interestRate/100)/12; 
-let numberOfPayment = loanTerm * 12; 
+let numberOfPayments = loanTerm * 12; 
 let numerator = montlyInterestRate * (1 + montlyInterestRate )**numberOfPayments; 
 let denominator = (1 + montlyInterestRate)**numberOfPayments -1; 
 let monthlyPayment = loanAmount * (numerator / denominator); 
@@ -21,11 +26,13 @@ let monthlyPayment = loanAmount * (numerator / denominator);
 let totalInterest = (monthlyPayment * numberOfPayments) - loanAmount; 
 
 //output format
+let outputField = document.getElementById("output"); 
 monthlyPayment=monthlyPayment.toFixed(2); 
 totalInterest = totalInterest.toFixed(2); 
 
 //Results 
-
 console.log("Monthly Payment: $"  + monthlyPayment); 
 console.log("Total Interest: $" + totalInterest); 
 
+}
+}
